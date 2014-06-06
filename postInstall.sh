43a162bc70f865feafc8dfa563a67e88084a89be
DJAE Mamode <infomamode@yahoo.fr>
2014-06-06 11:30:00 +0400

    Update postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -2,8 +2,9 @@
 #
 # mdjae - now
 # GPL
-#
-# 
+# Le script de post install vient après l'install :
+# EN MODE SERVER : Debian 7.0 Wheezy
+# EN MODE WORKSTATION : CrunchBang Linux 11 "Waldorf"
 
 VERSION="0.1"
 
@@ -21,7 +22,10 @@ fi
 sed -e 's/Port 22/Port 222/g' /etc/ssh/sshd_config
 sed -e 's/Protocol 1/Protocol 2/g' /etc/ssh/sshd_config
 sed -e 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-echo "AllowUsers user1 user2 user3"
+
+
+echo "AllowUsers ${OS_USERS[0]} ${OS_USERS[1]} ${OS_USERS[2]} ${OS_USERS[3]} ${OS_USERS[4]}" >> /etc/ssh/sshd_config
+
 
 # Ajout des depots
 
