b8d243600a86a6b6d4e72b3c85d42c78f8d2eee1
DJAE Mamode <infomamode@yahoo.fr>
2015-03-19 14:45:07 +0400

    Update postInstall.sh

--- postInstall.sh
+++ postInstall.sh
@@ -13,10 +13,9 @@ VERSION="0.1"
 source ./postInstall.config
 
 # Color bash prompt
-
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /root/.bashrc
 
-# Test que le script est lance en root
+# Test if script user is root
 if [ $EUID -ne 0 ]; then
   echo "========================================================================"
   echo "Le script doit être lancé en root: # sudo $0" 1>&2
@@ -25,8 +24,7 @@ if [ $EUID -ne 0 ]; then
 fi
 
 
-# Je suppose que SSH est déjà installé
-# SSH ============================================================
+# SSH service already here
 echo "========================================================================"
 echo "securisation SSH"
 echo "========================================================================"
@@ -56,9 +54,13 @@ sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 
 echo "AllowUsers ${SSH_USERS[0]} ${SSH_USERS[1]} ${SSH_USERS[2]} ${SSH_USERS[3]} ${SSH_USERS[4]}" >> /etc/ssh/sshd_config
 /etc/init.d/ssh restart
+
 # FAIL2BAN ============================================================
 apt-get install fail2ban
+
+
 # TODO : Configuration ....
+
 
 # PHP #PHP-FPM #PHP APC
 echo "================== ======================================================"
@@ -67,10 +69,19 @@ echo "========================================================================"
 apt-get install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn \
 php-pear php5-imagick php5-imap php5-json php5-mcrypt php5-memcache php5-mhash php5-ming \
 php5-mysql php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc \
-php5-xsl php5-fpm php5-mysql php5-apc
+php5-xsl php5-fpm php5-mysql 
+
+# PHP- APC
+apt-get install php5-apc
+
+# Configuraiton 
+# vim /etc/php5/apache2/php.ini
+# write before Module Settings section : extension=apc.so
+# write after Module Settings section : 
+# [APC] apc.enabled=1
+
 
 #PHP-FPM CONFIG
-
 sed -i 's/listen = 127.0.0.1:9000/;listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 /etc/init.d/php5-fpm restart
 
@@ -80,8 +91,10 @@ apt-get install mysql-server
 service mysql restart
 update-rc.d -f mysql enable
 
+
 #PHPMYADMIN
 apt-get install dbconfig-common phpmyadmin
+
 
 #PROFTPD
 apt-get install proftpd
@@ -119,63 +132,53 @@ openssl req -new -x509 -days 365 -nodes -out ftpd-rsa.pem -keyout ftpd-rsa-key.p
 # NGINX
 apt-get install nginx
 service nginx start
-/etc/nginx/conf.d/php5-fpm.conf
+# vim /etc/nginx/conf.d/php5-fpm.conf
  
 
-upstream php5-fpm-sock {
-        server unix:/var/run/php5-fpm.sock;
-}
+# upstream php5-fpm-sock {
+#         server unix:/var/run/php5-fpm.sock;
+# }
 
 
-/etc/nginx/sites-available/default
+# Write in /etc/nginx/sites-available/default
 
-server {
-        listen   80;
+# server {
+#         listen   80;
      
 
-        root /usr/share/nginx/www;
-        index index.php index.html index.htm;
+#         root /usr/share/nginx/www;
+#         index index.php index.html index.htm;
 
-        server_name XXX.com;
+#         server_name XXX.com;
 
-        location / {
-                try_files $uri $uri/ /index.html;
-        }
+#         location / {
+#                 try_files $uri $uri/ /index.html;
+#         }
 
-        error_page 404 /404.html;
+#         error_page 404 /404.html;
 
-        error_page 500 502 503 504 /50x.html;
-        location = /50x.html {
-              root /usr/share/nginx/www;
-        }
+#         error_page 500 502 503 504 /50x.html;
+#         location = /50x.html {
+#               root /usr/share/nginx/www;
+#         }
 
-        # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
-        location ~ \.php$ {
-                try_files $uri =404;
-                fastcgi_pass unix:/var/run/php5-fpm.sock;
-                fastcgi_index index.php;
-                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                include fastcgi_params;
-                
-        }
-
-}
+#         # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+#         location ~ \.php$ {
+#                 try_files $uri =404;
+#                 fastcgi_pass unix:/var/run/php5-fpm.sock;
+#                 fastcgi_index index.php;
+#                 fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+#                 include fastcgi_params;
+#                 
+#         }
+# 
+# }
 
 
 #  http://www.if-not-true-then-false.com/2011/nginx-and-php-fpm-configuration-and-optimizing-tips-and-tricks/
 
-# APC CONFIG
 
-/etc/php5/fpm/conf.d/apc.ini
-
-; configuration for php apc module
-extension=apc.so
-apc.shm_size=100
-
-/etc/init.d/php-fpm restart
-update-rc.d -f php5-fpm enable
-
-#MANGO REDIS
+# OrientDB
 
 # Paths Oracle 10g
 export PATH=$PATH:/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/bin:
